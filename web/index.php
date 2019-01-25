97a954ebf17e3183cf4a7c9a256ce0d3134a0eff
MerryGoblin <keller.alexandre0@gmail.com>
2019-01-25 10:32:38 +0100

    sample has been improved with comments and a better dump to see all the objects loaded

--- web/index.php
+++ web/index.php
@@ -2,17 +2,18 @@
 
 require_once(__DIR__."/../vendor/autoload.php");
 
+//	Parameters to connect on SQLite database
 $params = array(
 	'driver'    => 'pdo_sqlite',
 	'path'      => __DIR__."/../config/chinook.db",
 	'memory'    => false,
 );
 $config = new \Monolith\Casterlith\Configuration();
-$config->setSelectionReplacer("_cl");
+$config->setSelectionReplacer("_cl"); // The replacer insures that table's aliases won't be equal to real database's table names
 
-$orm            = new \Monolith\Casterlith\Casterlith($params, $config);
-$trackComposer  = $orm->getComposer('Acme\Composers\Track');
-$qb             = $trackComposer->getQueryBuilder();
+$orm            = new \Monolith\Casterlith\Casterlith($params, $config); //	Casterlith helps to create new instances of composers
+$trackComposer  = $orm->getComposer('Acme\Composers\Track'); //	Each table has its own composer
+$qb             = $trackComposer->getQueryBuilder(); //	DBAL's query builder can be accessed from Casterlith (a new instance) and from a Composer (same one as the one used by the composer)
 
 $tracks = $trackComposer
 	->select("t", "alb", "it", "g", "m", "pt", "p", "art", "inv", "c", "sub", "sup")
@@ -34,5 +35,10 @@ $tracks = $trackComposer
 	->setParameter('trackName', "%Princess%")
 	->setParameter('artistName', "Accept")
 	->all();
+
+// To see the entire dump you can uncomment the 3 lines below
+/*ini_set('xdebug.var_display_max_depth', '20');
+ini_set('xdebug.var_display_max_children', '65536');
+ini_set('xdebug.var_display_max_data', '1048576'); // 1048576*/
 
 var_dump($tracks);
