441ee747c4523d950c6ba004a720bafd7cff2eaa
Keller Alexandre <keller.alexandre0@gmail.com>
2019-12-24 18:36:16 +0100

    type of relation

--- tests/units/Casterlith.php
+++ tests/units/Casterlith.php
@@ -14,17 +14,87 @@ class Casterlith extends atoum
 
 	public function testContructorForSqlLite()
 	{
-		cleanSqlLiteDB();
-
 		$params = array(
 			'driver'  => 'pdo_sqlite',
-			'path'    => __DIR__."/config/sqlLite.db",
+			'path'    => __DIR__."/config/sqllite-unit-tests-readonly.db",
 			'memory'  => false,
 		);
 		$config = new \Monolith\Casterlith\Configuration();
 
 		$this
-			->given($casterlith = new \Monolith\Casterlith\Casterlith($params, $config))
+			->object($casterlith = new \Monolith\Casterlith\Casterlith($params, $config))
+				->isInstanceOf('\\Monolith\\Casterlith\\Casterlith')
 		;
 	}
+
+	public function testContructorForMySQL()
+	{
+		$params = array(
+			'driver'   => 'pdo_mysql',
+			'host'     => 'localhost',
+			'dbname'   => 'mysql-unit-tests-readonly',
+			'user'     => 'root',
+			'password' => '',
+		);
+		$config = new \Monolith\Casterlith\Configuration();
+
+		$this
+			->object($casterlith = new \Monolith\Casterlith\Casterlith($params, $config))
+				->isInstanceOf('\\Monolith\\Casterlith\\Casterlith')
+		;
+	}
+
+	public function testGetComposer()
+	{
+		$orm = getAReadOnlyOrmInstance();
+
+		$this
+			->object($artistComposer = $orm->getComposer('\\Acme\\Composers\\Artist'))
+				->isInstanceOf('\\Acme\\Composers\\Artist')
+		;
+	}
+
+	public function testGetComposerWithAnInvalidClass()
+	{
+		$orm = getAReadOnlyOrmInstance();
+
+		$this
+			->exception(
+				function() use($orm) {
+					$orm->getComposer('\\Acme\\Entities\\Artist');
+				}
+			)
+		;
+	}
+
+	public function testGetQueryBuilder()
+	{
+		$orm = getAReadOnlyOrmInstance();
+
+		$this
+			->object($queryBuilder = $orm->getQueryBuilder())
+				->isInstanceOf('\\Doctrine\\DBAL\\Query\\QueryBuilder')
+		;
+	}
+
+	public function testGetDBALConnection()
+	{
+		$orm = getAReadOnlyOrmInstance();
+
+		$this
+			->object($dbalConnection = $orm->getDBALConnection())
+				->isInstanceOf('\\Doctrine\\DBAL\\Connection')
+		;
+	}
+
+	public function testGetPDOConnection()
+	{
+		$orm = getAReadOnlyOrmInstance();
+
+		$this
+			->object($pdoConnection = $orm->getPDOConnection())
+				->isInstanceOf('\\PDO')
+		;
+	}
+
 }
--- tests/units/config/utils.php
+++ tests/units/config/utils.php
@@ -2,8 +2,22 @@
 
 function cleanSqlLiteDB()
 {
-	$fileName = __DIR__ . "/sqlLite.db";
+	$fileName = __DIR__ . "/sqllite-unit-tests-writable.db";
 	if (file_exists($fileName)) {
 		unlink($fileName);
 	}
+}
+
+function getAReadOnlyOrmInstance()
+{
+	$params = array(
+		'driver'  => 'pdo_sqlite',
+		'path'    => __DIR__."/sqllite-unit-tests-readonly.db",
+		'memory'  => false,
+	);
+	$config = new \Monolith\Casterlith\Configuration();
+
+	$casterlith = new \Monolith\Casterlith\Casterlith($params, $config);
+
+	return $casterlith;
 }
