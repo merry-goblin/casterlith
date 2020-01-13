b97d13f98fd0541986d43cf9c2167a8bcc280795
Keller Alexandre <keller.alexandre0@gmail.com>
2020-01-13 18:52:00 +0100

    More unit tests

--- tests/units/Schema/Builder.php
+++ tests/units/Schema/Builder.php
@@ -123,6 +123,210 @@ class Builder extends atoum
 		;
 	}
 
-	/*** select ***/
+	/*** from ***/
 
+	public function testFrom()
+	{
+		$orm = getAReadOnlyOrmInstance("types");
+		$queryBuilder = $orm->getQueryBuilder();
+		$selectionReplacer = "cl";
+		$schemaBuilder = new \Monolith\Casterlith\Schema\Builder($queryBuilder, $selectionReplacer);
+		$mapper = new \Monolith\Casterlith\tests\units\Schema\ArtistMapper();
+
+		$schemaBuilder->select("artist");
+		$schemaBuilder->from("artist", $mapper);
+
+		$num           = getPrivateValue($schemaBuilder, 'num');
+		$mapperList    = getPrivateValue($schemaBuilder, 'mapperList');
+		$selectionList = getPrivateValue($schemaBuilder, 'selectionList');
+		$jointList     = getPrivateValue($schemaBuilder, 'jointList');
+		$rootAlias     = getPrivateValue($schemaBuilder, 'rootAlias');
+
+		$this
+			->variable($num)
+				->isIdenticalTo(1)
+		;
+		$this
+			->array($mapperList)
+				->hasSize(1)
+				->hasKey("artist")
+		;
+		$this
+			->object($mapperList['artist'])
+				->isInstanceOf('\\Monolith\\Casterlith\\tests\\units\\Schema\\ArtistMapper')
+		;
+		$this
+			->array($selectionList)
+				->hasSize(1)
+				->hasKey("artist")
+		;
+		$this
+			->object($selectionList['artist'])
+				->isInstanceOf('\\Monolith\\Casterlith\\Schema\\Selection')
+		;
+		$this
+			->array($jointList)
+				->hasSize(0)
+		;
+		$this
+			->variable($rootAlias)
+				->isIdenticalTo("artist")
+		;
+	}
+
+	public function testFromWithTwoCalls()
+	{
+		$orm = getAReadOnlyOrmInstance("types");
+		$queryBuilder = $orm->getQueryBuilder();
+		$selectionReplacer = "cl";
+		$schemaBuilder = new \Monolith\Casterlith\Schema\Builder($queryBuilder, $selectionReplacer);
+		$mapper = new \Monolith\Casterlith\tests\units\Schema\ArtistMapper();
+
+		$schemaBuilder->select("artist");
+		$schemaBuilder->select("album");
+		$schemaBuilder->from("artist", $mapper);
+
+		$this
+			->exception(
+				function() use($schemaBuilder, $mapper) {
+					$schemaBuilder->from("album", $mapper);
+				}
+			)
+		;
+	}
+
+	public function testFromWithNonExistingAliasInSelection()
+	{
+		$orm = getAReadOnlyOrmInstance("types");
+		$queryBuilder = $orm->getQueryBuilder();
+		$selectionReplacer = "cl";
+		$schemaBuilder = new \Monolith\Casterlith\Schema\Builder($queryBuilder, $selectionReplacer);
+		$mapper = new \Monolith\Casterlith\tests\units\Schema\ArtistMapper();
+
+		$schemaBuilder->select("album");
+		$schemaBuilder->from("artist", $mapper);
+	}
+
+}
+
+//	Mapping
+
+class ArtistComposer extends \Monolith\Casterlith\Composer\AbstractComposer implements \Monolith\Casterlith\Composer\ComposerInterface
+{
+	protected static $mapperName  = '\\Monolith\\Casterlith\\tests\\units\\Schema\\ArtistMapper';
+}
+
+class AlbumComposer extends \Monolith\Casterlith\Composer\AbstractComposer implements \Monolith\Casterlith\Composer\ComposerInterface
+{
+	protected static $mapperName  = '\\Monolith\\Casterlith\\tests\\units\\Schema\\AlbumMapper';
+}
+
+class ArtistMapper extends \Monolith\Casterlith\Mapper\AbstractMapper implements \Monolith\Casterlith\Mapper\MapperInterface
+{
+	protected static $table      = 'artists';
+	protected static $entity     = '\\Monolith\\Casterlith\\tests\\units\\Schema\\ArtistEntity';
+	protected static $fields     = null;
+	protected static $relations  = null;
+
+	public static function getPrimaryKey()
+	{
+		return 'ArtistId';
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getFields()
+	{
+		if (is_null(self::$fields)) {
+			self::$fields = array(
+				'ArtistId'  => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+				'Name'      => array('type' => 'string'),
+			);
+		}
+
+		return self::$fields;
+	}
+
+	public static function getRelations()
+	{
+		if (is_null(self::$relations)) {
+			self::$relations = array(
+				'albums'            => new \Monolith\Casterlith\Relations\OneToMany(new \Monolith\Casterlith\tests\units\Schema\AlbumMapper(), 'artist', 'album', '`artist`.ArtistId = `album`.ArtistId', 'artist'),
+				'albumsNoRecursion' => new \Monolith\Casterlith\Relations\OneToMany(new \Monolith\Casterlith\tests\units\Schema\AlbumMapper(), 'artist', 'album', '`artist`.ArtistId = `album`.ArtistId'),
+			);
+		}
+
+		return self::$relations;
+	}
+}
+
+class AlbumMapper extends \Monolith\Casterlith\Mapper\AbstractMapper implements \Monolith\Casterlith\Mapper\MapperInterface
+{
+	protected static $table      = 'albums';
+	protected static $entity     = '\\Monolith\\Casterlith\\tests\\units\\Schema\\AlbumEntity';
+	protected static $fields     = null;
+	protected static $relations  = null;
+
+	public static function getPrimaryKey()
+	{
+		return 'AlbumId';
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getFields()
+	{
+		if (is_null(self::$fields)) {
+			self::$fields = array(
+				'AlbumId'   => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+				'Title'     => array('type' => 'string'),
+				'ArtistId'  => array('type' => 'integer'),
+			);
+		}
+
+		return self::$fields;
+	}
+
+	public static function getRelations()
+	{
+		if (is_null(self::$relations)) {
+			self::$relations = array(
+				'artist' => new \Monolith\Casterlith\Relations\ManyToOne(new \Monolith\Casterlith\tests\units\Schema\ArtistMapper(), 'album', 'artist', '`album`.ArtistId = `artist`.ArtistId', 'albums'),
+			);
+		}
+
+		return self::$relations;
+	}
+}
+
+
+class ArtistEntity implements \Monolith\Casterlith\Entity\EntityInterface
+{
+	public $ArtistId  = null;
+	public $Name      = null;
+
+	public $albums             = \Monolith\Casterlith\Casterlith::NOT_LOADED;
+	public $albumsNoRecursion  = \Monolith\Casterlith\Casterlith::NOT_LOADED;
+
+	public function getPrimaryValue()
+	{
+		return $this->ArtistId;
+	}
+}
+
+class AlbumEntity implements \Monolith\Casterlith\Entity\EntityInterface
+{
+	public $AlbumId   = null;
+	public $Title     = null;
+	public $ArtistId  = null;
+
+	public $tracks  = \Monolith\Casterlith\Casterlith::NOT_LOADED;
+	public $artist  = \Monolith\Casterlith\Casterlith::NOT_LOADED;
+
+	public function getPrimaryValue()
+	{
+		return $this->AlbumId;
+	}
 }
