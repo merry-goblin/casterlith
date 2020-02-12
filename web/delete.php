9a6ddf4d6ac680e3f3a0e65605ccce0b3e88a06a
Keller Alexandre <keller.alexandre0@gmail.com>
2020-02-12 13:46:36 +0100

    update and delete applies automatically a condition on the primary key

--- web/delete.php
+++ web/delete.php
@@ -5,33 +5,27 @@ require_once(__DIR__."/../vendor/autoload.php");
 //	Parameters to connect on SQLite database
 $params = array(
 	'driver'    => 'pdo_sqlite',
-	'path'      => __DIR__."/../config/chinook.db",
+	'path'      => __DIR__."/../config/chinook-insert.db",
 	'memory'    => false,
 );
 $config = new \Monolith\Casterlith\Configuration();
 $config->setSelectionReplacer("_cl"); // The replacer insures that table's aliases won't be equal to real database's table names
 
 $orm            = new \Monolith\Casterlith\Casterlith($params, $config);  // Casterlith helps to create new instances of composers
-$albumComposer  = $orm->getComposer('Acme\Composers\Album');              // Each table has its own query composer
+$trackComposer  = $orm->getComposer('Acme\Composers\Track');              // Each table has its own query composer
 
-$album = $albumComposer
+$track = $trackComposer
 	->select("t")
-	->where("t.AlbumId = :id")
-	->setParameter('id', 3)
+	->order("t.TrackId", "desc")
 	->first()
 ;
 
-$album->Title = "Restless and Wild (updated ".time().")";
-$fieldsToUpdate = array(
-	'Title',
-);
-
-$query = $albumComposer
-	->update($album, $fieldsToUpdate)
+$query = $trackComposer
+	->delete($track)
 ;
 
 if ($query->execute()) {
-	echo "Update is successful";
+	echo "Delete is successful";
 }
 else {
 	echo "An error occured";
